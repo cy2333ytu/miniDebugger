eb5c6f3bdf582ff03f69d9aacd8970960d90e947
ccy <ccy.com>
2024-03-12 10:32:42 +0800

    add class: Debugger

--- include/debugger.h
+++ include/debugger.h
@@ -2,6 +2,7 @@
 #define DEBUGGER_H
 
 #include <string>
+#include <vector>
 
 namespace ccy{
 
@@ -11,6 +12,10 @@ public:
             m_prog_name{prog_name}, m_pid{pid}{}
 
     void run();
+    void handleCommand(const std::string& line);
+    std::vector<std::string> split(const std::string&s, char delimiter);
+    void continueExection();
+    bool isPrefix(const std::string&s, const std::string& of);
 private:
     std::string m_prog_name;
     pid_t m_pid;
--- src/debugger.cc
+++ src/debugger.cc
@@ -1,7 +1,14 @@
-#include<sys/types.h>
-#include<sys/wait.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <fcntl.h>
+#include <stdexcept>
+#include<sys/ptrace.h>
 
-#include"linenoise.h"
+#include "linenoise.h"
 #include "debugger.h"
 namespace ccy
 {
@@ -12,10 +19,43 @@ void Debugger::run(){
     waitpid(m_pid, &wait_status, options);
 
     char *line = nullptr;
-    while((line == linenoise("minidbg> ")) != nullptr){
+    while((line = linenoise("minidbg> ")) != nullptr){
         handleCommand(line);
         linenoiseHistoryAdd(line);
         linenoiseFree(line);
     }
 }
+
+void Debugger::handleCommand(const std::string& line){
+    auto args = split(line, ' ');
+    auto command = args[0];
+    if(isPrefix(command, "c")){
+        continueExection();
+    }else{
+        std::cerr << "Unknow command\n";
+    }
+}
+
+std::vector<std::string> Debugger::split(const std::string&s, char delimiter){
+    std::vector<std::string> res{};
+    std::stringstream ss{s};
+    std::string item;
+    while(std::getline(ss, item, delimiter)){
+        res.emplace_back(item);
+    }
+    return res;
+}
+
+bool Debugger::isPrefix(const std::string&s, const std::string& of){
+    if(s.size() > of.size()) return false;
+    return std::equal(s.begin(), s.end(), of.begin());
+}
+
+void Debugger::continueExection(){
+    ptrace(PTRACE_CONT, m_pid, nullptr, nullptr);
+    int wait_status;
+    auto options = 0;
+    waitpid(m_pid, &wait_status, options);
+}
+
 }
