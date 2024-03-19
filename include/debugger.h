0b2ffac8421124a7102ec2da6a1eafa7b6acd89e
ccy <ccy.com>
2024-03-19 10:16:53 +0800

    add func: initAddress offsetAddress waitForSignal

--- include/debugger.h
+++ include/debugger.h
@@ -17,18 +17,26 @@ public:
     Debugger(std::string prog_name, pid_t pid);
 
     void run();
+    void waitForSignal();
+    void initializeLoadAddress();
+    uint64_t offsetLoadAddress(uint64_t addr);
+    void waitForSignal();
+    siginfo_t getSignalInfo();
+
     void handleCommand(const std::string& line);
     std::vector<std::string> split(const std::string&s, char delimiter);
     void continueExection();
     bool isPrefix(const std::string&s, const std::string& of);
     void setBreakpointAtAddress(std::intptr_t addr);
     dwarf::die getFunctionFromPc(uint64_t pc);
+    dwarf::line_table::iterator getLineEntryFromPc(uint64_t pc);
 private:
     std::string m_prog_name;
     pid_t m_pid;
     std::unordered_map<std::intptr_t, std::unique_ptr<Breakpoint>> m_breakpoint;
     elf::elf m_pElf;          // Executable and Linkable Format
     dwarf::dwarf m_pDwarf;    // Debugging With Arbitrary Record Format
+    uint64_t m_loadAddress = 0;
 };
 
 
--- src/debugger.cc
+++ src/debugger.cc
@@ -37,6 +37,44 @@ void Debugger::run(){
     }
 }
 
+void Debugger::waitForSignal(){
+
+}
+
+void Debugger::initializeLoadAddress() {
+    if (m_pElf.get_hdr().type == elf::et::dyn) {
+        std::ifstream map;
+        std::string map_entry;
+        std::stringstream ss;
+
+        // 打开maps文件
+        map.open("/proc/" + std::to_string(m_pid) + "/maps");
+        if (map.is_open()) {
+            // 读取第一行
+            if (std::getline(map, map_entry)) {
+                size_t dash_pos = map_entry.find('-');
+                if (dash_pos != std::string::npos) {
+                    std::string addr_str = map_entry.substr(0, dash_pos);
+                    // 使用stringstream来进行转换
+                    ss << std::hex << addr_str;
+                    // 用合适的类型来处理64位地址
+                    ss >> m_loadAddress;
+                } else {
+                    // 处理maps文件格式异常
+                    std::cerr << "Error: Unexpected format in /proc/[pid]/maps." << std::endl;
+                }
+            } else {
+                // 处理读取失败
+                std::cerr << "Error: Unable to read /proc/[pid]/maps." << std::endl;
+            }
+            // 关闭文件
+            map.close();
+        } else {
+            // 处理文件打开失败
+            std::cerr << "Error: Unable to open /proc/[pid]/maps." << std::endl;
+        }
+    }
+}
 void Debugger::setBreakpointAtAddress(std::intptr_t addr){
     std::cout << "Set breakpoint at address 0x" << std::hex << addr << std::endl;
     auto bp = std::make_unique<Breakpoint>(m_pid, addr);
@@ -94,6 +132,49 @@ dwarf::die Debugger::getFunctionFromPc(uint64_t pc){
     }
     throw std::out_of_range{"Cannot find function"};
 
+}
+dwarf::line_table::iterator Debugger::getLineEntryFromPc(uint64_t pc){
+    for(auto& compilationUnit: m_pDwarf.compilation_units()){
+        if(dwarf::die_pc_range(compilationUnit.root()).contains(pc)){
+            auto It = compilationUnit.get_line_table();
+            auto it = It.find_address(pc);
+            if(it == It.end()){
+                throw std::out_of_range{"Cannot find line entry"};
+            }else{
+                return it;
+            }
+        }
+    }
+    throw std::out_of_range("Cannot find line entry");
+}
+
+uint64_t Debugger::offsetLoadAddress(uint64_t addr){
+    return addr - m_loadAddress;
+}
+
+siginfo_t Debugger::getSignalInfo(){
+    siginfo_t info;
+    ptrace(PTRACE_GETSIGINFO, m_pid, nullptr, &info);
+    return info;
+}
+
+void Debugger::waitForSignal(){
+    int waitStatus;
+    auto options = 0;
+    waitpid(m_pid, &waitStatus, options);
+    auto siginfo = getSignalInfo();
+
+    switch(siginfo.si_signo){
+        case SIGTRAP:
+            handleSignalTrap(siginfo);
+            break; 
+        case SIGSEGV:
+            std::cout << "Yay, segfault. Reason: " << siginfo.si_code << std::endl;
+            break;
+        default:
+            std::cout << "Got signal " << strsignal(siginfo.si_signo) << std::endl;
+        
+    }
 }
 
 }
